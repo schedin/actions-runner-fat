4b020eb43a34e3076fd0be0e107914a4b689c68c
Lennart Schedin <lennart.schedin@gmail.com>
2025-05-14 12:21:31 +0200

    test

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -9,13 +9,13 @@ if ! skopeo  2>/dev/null; then
 fi
 
 # Creating fake docker.sock to podman.sock file so GitHub Actions image builder works.
-sudo mkdir -p /run/podman
-sudo touch /run/podman/podman.sock
+#sudo mkdir -p /run/podman
+#sudo touch /run/podman/podman.sock
 
 # Create GitHub action directories if they don't exist
 # This helps when running Docker actions with Podman
-echo "Running mkdir -p ${HOME}/_work/_temp/_github_home"
-mkdir -p ${HOME}/_work/_temp/_github_home
+#echo "Running mkdir -p ${HOME}/_work/_temp/_github_home"
+#mkdir -p ${HOME}/_work/_temp/_github_home
 
 # Execute the command passed to the container
 exec "$@"
