48ac88e5a34352b2854e420219726d1b06106b1f
Lennart Schedin <lennart.schedin@gmail.com>
2025-05-14 10:49:52 +0200

    New workaround for skopeo

---
+++ image/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Test if skopeo works with current capabilities
+if ! skopeo  2>/dev/null; then
+  # When running inside a normal Podman container with --device /dev/fuse we
+  # need to remove the CAP_SYS_ADMIN capability from skopeo
+  sudo setcap cap_sys_admin-ep /usr/bin/skopeo
+fi
+
+# Execute the command passed to the container
+exec "$@"
